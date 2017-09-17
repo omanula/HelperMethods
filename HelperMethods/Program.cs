0a325a223739ab03b8e684dae846ca1bcb83ff72
Olivia <31357493+omanula@users.noreply.github.com>
2017-09-17 16:50:39 -0600

    Helper Methods (Part 1)

---
+++ HelperMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelperMethods
+{
+    class MainClass
+    {
+        // A method is a code block with a name
+        // Concept - we can offload certain operation to other
+                  // methods to organize and reuse our code. 
+
+        public static void Main(string[] args)
+        {
+            string myValue = superSecretFormula();
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+        }
+
+        // the method MUST be inside class, after Main string
+        private static string superSecretFormula()
+        // private: can only be called within this class
+        // public: can be called from outside the class
+        // string: data type for the return of this method
+        {
+            // some cool stuff here
+            return "Hello World";
+            // return: command to deliver results back to method 
+            // that called it.
+        }
+
+    }
+}
